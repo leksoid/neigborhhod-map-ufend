1813a6b3ef571461435c42b4f7fc8b121543a7f9
Taran, Aliaksei (Consultant) <aliaksei.taran@gmail.com>
2018-10-26 16:58:21 -0400

    Make map load

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,41 @@
 import React, {Component} from 'react'
 
 class Map extends Component{
+    constructor(props){
+        super(props);
+        this.mapRef = React.createRef();
+    }
+
+    state = {
+        isScriptReady: false,
+    };
+
+    componentDidMount() {
+        const ApiKey = 'AIzaSyCdH-NvXfoWzdwnhd2xYP7DsIQ54kSRAic';
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${ApiKey}`;
+        script.async = true;
+        script.defer = true;
+        script.addEventListener('load', () => {
+            this.setState({ isScriptReady: true });
+        });
+        document.body.appendChild(script);
+    }
+
+    componentDidUpdate() {
+        if (this.state.isScriptReady) {
+            let map = new window.google.maps.Map(this.mapRef.current, {
+                center: {lat: 41.0085382, lng: -73.8683036},
+                zoom: 12,
+            });
+        }
+    }
+
+
     render(){
         return(
-            <div className='map'></div>
+            <div className='map' ref={this.mapRef}>
+            </div>
         )
     }
 }

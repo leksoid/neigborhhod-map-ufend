3771fb492583993610f86007daef34e391474f9e
Taran, Aliaksei (Consultant) <aliaksei.taran@gmail.com>
2018-11-04 01:00:02 -0400

    Added styles to map

--- src/Map.js
+++ src/Map.js
@@ -4,6 +4,96 @@ const ApiKey = 'AIzaSyCdH-NvXfoWzdwnhd2xYP7DsIQ54kSRAic';
 const FAPI_CLIENT_ID = 'H4MWPFTV2MD22GEGS2HYNECXWWNOQBU0AHIB5PSJG0U4LRN4';
 const FAPI_CLIENT_SECRET = 'FEJNQMXTTUJBI1MCVMIRKGAGRAUKFSLR423UKN0LSII3YTT1';
 let map;
+let style = [
+    {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "color": "#dec55c"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 65
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "color": "#fcb32c"
+            },
+            {
+                "saturation": -100
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#5fa7cd"
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+]
 
 class Map extends Component{
     constructor(props){
@@ -15,13 +105,24 @@ class Map extends Component{
         isScriptReady: false,
     };
 
-    displayFInfo = (marker) => {
+    getVenueId = (marker) => {
+        let venueID = null;
         let p = marker.getPosition().toUrlValue();
-        let request = new Request(`https://api.foursquare.com/v2/venues/search?ll=${p}&client_id=${FAPI_CLIENT_ID}&client_secret=${FAPI_CLIENT_SECRET}&llAcc=10&radius=100&v=20181029`,{
+        let request = new Request(`https://api.foursquare.com/v2/venues/search?ll=${p}&client_id=${FAPI_CLIENT_ID}&query=${marker.title}&client_secret=${FAPI_CLIENT_SECRET}&llAcc=1000&radius=1000&v=20181029&limit=1`,{
             method: 'GET'
         });
-        fetch(request).then((response) => console.log(response.json()));
+        fetch(request)
+            .then((response) => response.json())
+            .then((result) => {
+                venueID = result.response.venues.length > 0 ? result.response.venues[0].id : "No venue found in DB";
+                console.log(venueID);
+            });
+        return venueID;
     };
+
+    displayFoursquareData = (marker) => {
+
+    }
 
     componentDidMount() {
         const script = document.createElement('script');
@@ -40,14 +141,15 @@ class Map extends Component{
             let markers =[];
             let locations = [
                 {title: 'Riviera Bakehouse', location: {"lat": 41.0111915, "lng": -73.8486300}},
-                {title: 'By the Way Bakery', location: {lat: 40.9974559, lng: -73.8778848}},
-                {title: 'Red Barn Bakery', location: {lat: 41.038161, lng: -73.870015}},
-                {title: 'Domenicks Nepperhan Italian', location: {lat: 40.974258, lng: -73.86907}},
-                {title: 'Lulu Cake Boutique', location: {lat: 41.0032598, lng: -73.8564513}}
+                {title: 'By The Way Bakery', location: {"lat": 40.9974559, "lng": -73.8778848}},
+                {title: 'Red Barn Bakery', location: {"lat": 41.038161, "lng": -73.870015}},
+                {title: 'Domenicks Nepperhan Italian', location: {"lat": 40.974258, "lng": -73.86907}},
+                {title: 'Lulu Cake Boutique', location: {"lat": 41.0032598, "lng": -73.8564513}}
             ];
             map = new window.google.maps.Map(this.mapRef.current, {
                 center: {lat: this.props.lat, lng: this.props.lng},
                 zoom: this.props.zoom,
+                styles: style,
             });
             for (let i = 0; i < locations.length; i++) {
                 let position = locations[i].location;
@@ -67,7 +169,7 @@ class Map extends Component{
                 marker.addListener('click', ()=> {
                     console.log(marker.position);
                     info.open(map, marker);
-                    this.displayFInfo(marker);
+                    this.getVenueId(marker);
                 });
             }
             map.fitBounds(bounds);
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,7 +5,11 @@ class Sidebar extends Component{
     render(){
         return(
             <div className='sidebar'>
-                <div className='header'></div>
+                <div className='header'>
+                    <h1>
+                        Westchester Bakeries
+                    </h1>
+                </div>
                 <SearchList/>
                 <div className='footer'></div>
             </div>
